2420a51f38166444f5f26c8ffd99f1d3fc57d060
Cristian Marin <cristian@mac-cmarin-mx.local>
2017-07-06 18:47:16 -0500

    Corrected git server, entrypoint and docker compose environmet files

--- wp-csi-plugin-dockerfile/docker-entrypoint.sh
+++ wp-csi-plugin-dockerfile/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 if ! [ -d csi-wp-plugin ]; then
-	git clone https://github.com/tianmarin/csi-wp-plugin.git && chmod -R 777 csi-wp-plugin
+	git clone https://${BBUSER}:${BBPASS}@bitbucket.org/novismx/csi-wp-plugin.git && chmod -R 777 csi-wp-plugin
 fi
 cd csi-wp-plugin
-npm install >npm_install.log
+#npm install >npm_install.log
 
-gulp browser-sync >> gulp.log
+npm run gulp browser-sync >> gulp.log
 
