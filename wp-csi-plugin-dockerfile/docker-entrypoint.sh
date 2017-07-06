ef2e9b81136dfd20ef66b3bb73caabf6a675028f
Cristian Marin <cristian@mac-cmarin-mx.local>
2017-07-06 17:08:44 -0500

    Fix editor container dockerfiles

--- wp-csi-plugin-dockerfile/docker-entrypoint.sh
+++ wp-csi-plugin-dockerfile/docker-entrypoint.sh
@@ -5,5 +5,7 @@ if ! [ -d csi-wp-plugin ]; then
 	git clone https://github.com/tianmarin/csi-wp-plugin.git && chmod -R 777 csi-wp-plugin
 fi
 cd csi-wp-plugin
-npm install
-gulp browser-sync
+npm install >npm_install.log
+
+gulp browser-sync >> gulp.log
+
