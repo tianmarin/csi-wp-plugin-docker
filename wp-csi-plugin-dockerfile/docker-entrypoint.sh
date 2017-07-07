668690afa3e9f831e865d6699163080af8d78f34
Cristian Marin <cristian@mac-cmarin-mx.local>
2017-07-07 15:33:47 -0500

    ESB Template and split of environment files

--- wp-csi-plugin-dockerfile/docker-entrypoint.sh
+++ wp-csi-plugin-dockerfile/docker-entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -euo pipefail
+cd /themes
+if ![ -d empire-strikes-back-wp-template ]; then
+        git clone https://${BBUSER}:${BBPASS}@bitbucket.org/novismx/empire-strikes-back-wp-template.git && chmod -R 777 empire-strikes-back-wp-template
 
+fi
+
+cd /plugins
 if ! [ -d csi-wp-plugin ]; then
 	git clone https://${BBUSER}:${BBPASS}@bitbucket.org/novismx/csi-wp-plugin.git && chmod -R 777 csi-wp-plugin
 fi
-cd csi-wp-plugin
-#npm install >npm_install.log
+cd csi-wp-plugin/
+npm config set cache ./.npm
 
-npm run gulp browser-sync >> gulp.log
+
+#npm install >> npm_install.log
+npm install > /proc/1/fd/1 2>&1
+
+npm run gulp browser-sync > /proc/1/fd/1 2>&1
 
